65a13866212b70a25bf6bbdc43b573a158bc46cf
Soham Ganguly <sohamg24@gmail.com>
2024-04-09 14:04:16 -0400

    add login card component

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPrompt from "../components/LoginPrompt";
+
+const LoginPage = () => {
+  return (
+    <div className="p-8">
+      <LoginPrompt />
+    </div>
+  );
+};
+
+export default LoginPage;
