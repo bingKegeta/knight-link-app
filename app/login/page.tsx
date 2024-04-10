e3cecfa5c80a3a37e85d44e06e3826a10da49218
Soham Ganguly <sohamg24@gmail.com>
2024-04-09 23:45:18 -0400

    new login prompt and page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,10 @@ import LoginPrompt from "../components/LoginPrompt";
 
 const LoginPage = () => {
   return (
-    <div className="p-8">
-      <LoginPrompt />
+    <div className=" h-[100vh] flex items-center justify-center m-auto bg-gradient-to-br from-base-100 to-indigo-800 via-neutral animate-gradient-x">
+      <div className="p-8 card card-normal bg-base-100 fixed border-2 border-primary inline-block">
+        <LoginPrompt />
+      </div>
     </div>
   );
 };
