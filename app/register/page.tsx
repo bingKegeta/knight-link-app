611a3bac9baf8498923e1734abaff9db2f9feb3e
Soham Ganguly <sohamg24@gmail.com>
2024-04-10 00:15:46 -0400

    add register page and prompt

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,8 +3,10 @@ import RegisterPrompt from "../components/RegisterPrompt";
 
 const RegisterPage = () => {
   return (
-    <div className="p-4">
-      <RegisterPrompt />
+    <div className=" h-[100vh] flex items-center justify-center m-auto bg-gradient-to-br from-base-100 to-indigo-800 via-neutral animate-gradient-x">
+      <div className="p-8 card card-normal bg-base-100 fixed border-2 border-primary inline-block">
+        <RegisterPrompt />
+      </div>
     </div>
   );
 };
