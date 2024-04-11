a74f6125a759da55b99b530bce46478833d39d6e
Alejandro Palmar <52765355+alejoseed@users.noreply.github.com>
2024-04-11 19:14:17 -0400

    Replaced page styling to layout. Login form works as intended

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,131 +1,193 @@
-"use client";
+'use client'
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React from "react";
-import { useForm } from "react-hook-form";
-import LoginErrorAlert from "./LoginErrorAlert";
-import { debug } from "console";
+import React, { useEffect, useState } from "react";
+import { FieldErrors, UseFormRegister, useForm } from "react-hook-form";
+import { useFormState, useFormStatus } from "react-dom";
+import { LoginUser, State } from "./server/actions";
+import CloseBtn from "./CloseButton";
 
-interface LoginProps {
+interface LoginInputs {
   username: string;
   password: string;
 }
 
+export function LoginFormContent({
+  register,
+  isValid,
+  errors,
+}: {
+  register: UseFormRegister<LoginInputs>;
+  isValid: boolean;
+  errors: FieldErrors<LoginInputs>;
+}) {
+  
+  const { pending } = useFormStatus();
+
+    return (
+      <>
+        <span className={`loading loading-spinner text-success w-16 fixed top-2/4 left-2/4 -translate-x-1/2 
+                          ${pending ? "opacity-100" : "opacity-0 backdrop-blur-sm transition-opacity"}`}></span>
+
+        <div className={`md:p-8 lg:card-normal p-4
+                          card bg-base-100 border-2 border-primary
+                          ${pending ? "opacity-0" : "opacity-100 backdrop-blur-sm transition-opacity"}
+                          `}>
+          <CloseBtn />
+          <div className="card-body">
+              {/* Username field */}
+              <div className="form-control">
+                <label className="input input-bordered flex items-center gap-2 focus:input-secondary">
+                  {/* Username svg icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70"
+                  >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                  </svg>
+                  <input
+                  type="text"
+                  placeholder="Username"
+                  className={`grow`}
+                  {...register("username", { required: "Username is required" })}
+                />
+                </label>
+
+                {errors.username && (
+                  <span className="label-text-alt text-error">{errors.username.message}</span>
+                )}
+              </div>
+
+              {/* Password field */}
+              <div className="form-control">
+              <label className="input input-bordered flex items-center gap-2 focus:input-secondary">
+                {/* Password svg */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <input
+                      type="password"
+                      placeholder="Password"
+                      className={`grow`}
+                      {...register("password", { required: "Password is required" })}
+                    />
+                </label>
+                
+                {errors.password && (
+                  <span className="label-text-alt text-error">{errors.password.message}</span>
+                )}
+              </div>
+
+              {/* Forgot Password Link */}
+              <div className="form-control">
+                <Link href="/" className="label-text-alt link link-hover">
+                  Forgot password?
+                </Link>
+              </div>
+
+              {/* Submit button */}
+              <div className="form-control mt-6">
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </div>
+
+            {/* Sign Up Link */}
+            <div className="form-control">
+              <Link href="/register" className="link link-hover">
+                Don't have an account yet? Click here to sign up!
+              </Link>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+}
+
+
 export default function LoginForm() {
-  const router = useRouter();
   const {
     register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginProps>();
+    formState: { isValid, errors },
+  } = useForm<LoginInputs>();
 
-  async function onSubmit(data: LoginProps) {
-    debugger;
-    try {
-      const response = await fetch("http://localhost:8000/v1/api/auth/login", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+  const [state, formAction] = useFormState<State, FormData>(LoginUser, null);
+  const [alert, setAlert] = useState<{ visible: boolean, content: React.JSX.Element | null }>({ visible: false, content: null });
 
-      if (!response.ok) {
-        throw new Error('Login request failed: ' + response.statusText);
-      }
 
-      // Extracting the token from the response
-      const tokenData = await response.json();
-      const token = tokenData.token;
+  useEffect(() => {
+    if (state) {
+      setAlert({ visible: true, content: alertCreation(state) });
+    
+      const fadeOutTimer = setTimeout(() => {
+        setAlert(prevState => ({ ...prevState, visible: false }));
+      }, 5000);
 
-      if (!token) {
-        throw new Error('Token not received');
-      }
-      
-      localStorage.setItem('token', token);
-      console.log('Login successful, token:', token);
+
+      const removeTimer = setTimeout(() => {
+        setAlert({ visible: false, content: null });
+      }, 6000);
+
+      return () => {
+        clearTimeout(fadeOutTimer);
+        clearTimeout(removeTimer);
+      };
     }
-
-    catch (ex) {
-      console.log(ex);
-      return;
-    }
-
-    // const res = await fetch("/api/auth", {
-    //   method: "POST",
-    //   body: JSON.stringify(data),
-    //   headers: {
-    //     "content-type": "application/json",
-    //   },
-    // });
-
-    // console.log(res.json());
-
-    // if (!res.ok) {
-    //   return <LoginErrorAlert />;
-    // } else {
-    //   console.log(res.json());
-    // }
-  }
+  }, [state]);
 
   return (
-    <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-control">
-        <label className="input input-bordered flex items-center gap-2 focus:input-secondary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-          </svg>
-          <input
-            type="text"
-            className="grow"
-            placeholder="Username"
-            {...register("username", {
-              required: true,
-            })}
-          />
-        </label>
-      </div>
-      <div className="form-control">
-        <label className="input input-bordered flex items-center gap-2 focus:input-secondary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <input
-            type="password"
-            className="grow"
-            placeholder="password"
-            {...register("password", {
-              required: true,
-            })}
-          />
-        </label>
-        <label className="label">
-          <Link href="/" className="label-text-alt link link-hover">
-            Forgot password?
-          </Link>
-        </label>
-      </div>
-      <div className="form-control mt-6">
-        <button className="btn btn-primary">Login</button>
-      </div>
-      <Link href="/register" className="form-control">
-        Don&apos;t have an account yet? 
-        Click here to sign up!
-      </Link>
-    </form>
+    <div>
+      <form action={formAction} >
+        <LoginFormContent register={register} isValid={isValid} errors={errors} />
+      </form>
+      <div className={`transition-opacity duration-200 fixed bottom-0 right-0 m-4 ${alert.visible ? 'opacity-100' : 'opacity-0'}`}>
+        {alert.content}
+    </div>
+    </div>
   );
 }
+
+
+function alertCreation(state : State) {
+  let alertComponent = null;
+
+  if (state?.status === "success") {
+    alertComponent = (
+      <div role="alert" className="alert alert-success">
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>{state?.message || "User created!"}</span>
+      </div>
+    );
+  } 
+
+  else if (state?.status === "warning") {
+    alertComponent = (
+      <div role="alert" className="alert alert-warning">
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+        <span>{state?.message || "Warning: There was an unexpected error!"}</span>
+      </div>
+    );
+  } 
+  
+  else if (state?.status === "error") {
+    alertComponent = (
+      <div role="alert" className="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>{state?.message || "Error! Task failed."}</span>
+      </div>
+    );
+  }
+
+  return alertComponent;
+}
+
--- app/components/server/actions.ts
+++ app/components/server/actions.ts
@@ -56,3 +56,51 @@ export async function RegisterUser(prevState: State | null, formData: FormData):
     }
 }
 
+
+export async function LoginUser(prevState: State | null, formData: FormData): Promise<State> {
+  try {
+    const data: Partial<Record<string, string>> = {};
+    
+    formData.forEach((value, key) => { 
+      data[key] = value.toString();
+    });
+
+    const response = await fetch("http://localhost:8000/v1/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const responseText = await response.text();
+
+    let responseBody;
+
+    try {
+      responseBody = JSON.parse(responseText);
+    } catch (e) {
+      responseBody = responseText;
+    }
+    
+    if (!response.ok) {
+      const errorMessage = typeof responseBody === 'object' ? responseBody.message : responseBody;
+      return {
+          status: "warning",
+          message: `Login failed: ${errorMessage}`
+      };
+    }
+
+    return {
+      status: "success",
+      message: typeof responseBody === 'object' ? responseBody.message : responseBody
+    };
+  }
+
+  catch (error : any) {
+      return {
+        status: "error",
+        message: `Login failed: ${error.message || error}`
+    };
+  }
+}
