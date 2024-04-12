766aff0da199169d7f21d4ac1a17608c409450bd
Alejandro Palmar <52765355+alejoseed@users.noreply.github.com>
2024-04-11 23:46:40 -0400

    Added event page, fixed styling on home page to grid

---
+++ app/event/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventCard from "../components/EventCard";
+import { EventProps } from "../helpers/interfaces";
+
+
+export default function Event() {
+  return (
+    <div>
+      Some info here
+    </div>
+  )
+}
