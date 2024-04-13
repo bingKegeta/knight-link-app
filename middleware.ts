b1c59f06db26f262e96b4b273136b6bda080c651
Soham Ganguly <sohamg24@gmail.com>
2024-04-13 10:47:30 -0400

    fix going to register without auth redirect

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get('username')?.value;
+  const currentUser = request.cookies.get("username")?.value;
   const { pathname } = request.nextUrl;
 
-  if (pathname === '/') {
+  if (pathname === "/") {
     return;
   }
 
   // Redirect logged-in users to /home if they're not already there
-  if (currentUser && pathname === '/login') {
-    return Response.redirect(new URL('/home', request.url));
+  if (currentUser && pathname === "/login") {
+    return Response.redirect(new URL("/home", request.url));
   }
 
   // Redirect non-logged-in users to /login if they're trying to access any other path
-  if (!currentUser && pathname !== '/login') {
-    return Response.redirect(new URL('/login', request.url));
+  if (!currentUser && pathname !== "/login" && pathname !== "/register") {
+    return Response.redirect(new URL("/login", request.url));
   }
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-}
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
