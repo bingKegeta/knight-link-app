7622ddcf182368351bb47a95abe1cbfab48d57c6
Alejandro Palmar <52765355+alejoseed@users.noreply.github.com>
2024-04-13 09:25:46 -0400

    Added middleware for routing

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get('username')?.value;
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/') {
+    return;
+  }
+
+  // Redirect logged-in users to /home if they're not already there
+  if (currentUser && pathname === '/login') {
+    return Response.redirect(new URL('/home', request.url));
+  }
+
+  // Redirect non-logged-in users to /login if they're trying to access any other path
+  if (!currentUser && pathname !== '/login') {
+    return Response.redirect(new URL('/login', request.url));
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
